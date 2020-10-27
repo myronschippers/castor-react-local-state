3206eeb6b6260ecd4dda4f2c8a89e95e9d73d193
Myron <dubtwee@gmail.com>
2020-10-27 15:05:05 -0500

    creating local state for input field

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,45 @@ import './App.css';
 import Header from '../Header/Header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employee: 'Bob',
+    };
+  }
+
+  // EVENT LISTENER
+  // $('.selector').on('change', handleClick)
+
+  handleNameChange = (event) => {
+    // DO NOT DO
+    // this.state.employee = event.target.value;
+
+    // SETTING STATE
+    this.setState(
+      {
+        employee: event.target.value,
+      },
+      () => {
+        console.log('Changed', this.state.employee);
+      }
+    );
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <main className="container">MAIN CONTENT</main>
+        <main className="container">
+          <input
+            type="text"
+            placeholder="Your Name?"
+            name="name"
+            onChange={this.handleNameChange}
+          />
+          <span>My employee is {this.state.employee}</span>
+        </main>
       </div>
     );
   }
